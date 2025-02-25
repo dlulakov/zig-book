d3d69ef601806e1d6a811d1c9ae2ae07c69aa4fa
dlulakov <d.lulakov@gmail.com>
2025-02-25 22:54:02 +0100

    Added the changes I made while testing on 2

---
+++ 2.Control_flow/struct/src/main.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+
+const User = struct {
+    id: u64,
+    name: []const u8,
+    email: []const u8,
+    pub fn init(id: u64,name: []const u8,email: []const u8)User{
+        return User{
+            .id = id,
+            .name = name,
+            .email = email
+        };
+    }
+    
+    pub fn print_name(self: User) !void {
+       try stdout.print("{s}\n", .{self.name});
+    }
+};
+
+
+pub fn main() !void {
+    const u = User.init(1, "Damjan", "d.lulakov@gmail.com");
+    try u.print_name();
+}
---
+++ 2.Control_flow/switch/src/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const Role = enum {
+    SE,
+    DPE,
+    DE,
+    DA,
+    PM,
+    PO,
+    KS
+};
+
+pub fn main() !void {
+    var area: []const u8 = undefined;
+    const role: Role = Role.SE;
+    switch (role) {
+        .PM, .SE, .DPE, .PO => { 
+            area = "Platform";
+        },
+        .DE, .DA => {
+            area = "Data & Analytics";
+        },
+        .KS => {
+            area = "Sales";
+        }
+    }
+    try stdout.print("{s}", .{area});
+}
